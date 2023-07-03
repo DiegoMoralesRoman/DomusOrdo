0e0bbc078ddac5db35140aa1a3d6f288a14d2319
Diego Morales Román <moralesromandiego@gmail.com>
2023-07-03 02:12:53 +0200

    Added reverse proxy to the service

--- web/API/app/__init__.py
+++ web/API/app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from .routes import register_routes
-import os
 from .dbmodel import db
+
+class LoggingMiddleware:
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, environ, start_response):
+        print(f'Received request: {environ["REQUEST_METHOD"]} {environ["PATH_INFO"]}')
+        return self._app(environ, start_response)
 
 def create_app(config_path: str = 'config.py') -> Flask:
     app = Flask(__name__)
@@ -17,4 +24,7 @@ def create_app(config_path: str = 'config.py') -> Flask:
     # Register routes
     register_routes(app)
 
+    # Add logging
+    wsgi_app = LoggingMiddleware(app.wsgi_app)
+    app.wsgi_app = wsgi_app
     return app
--- web/API/app/routes/index.py
+++ web/API/app/routes/index.py
@@ -8,12 +8,5 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    projects = Project.query.all()
-    for proj in projects:
-        print(proj.description)
-    return render_template('index.html',
-        projects=projects
-    )
-
-
+    return {'name': 'test'}
 
